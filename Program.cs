bc0be8f41d7782d9f42a55b708e62a4d6bd72129
Reef-hash <riffsyaz@gmail.com>
2026-04-30 12:52:01 -0700

    Remove duplicate Main entry point for WPF (fix CS0017)

--- Program.cs
+++ Program.cs
@@ -1,16 +1,1 @@
-using System;
-using System.Windows;
 
-namespace EZPos
-{
-    public partial class Program
-    {
-        [STAThread]
-        public static void Main()
-        {
-            var app = new App();
-            app.InitializeComponent();
-            app.Run();
-        }
-    }
-}
