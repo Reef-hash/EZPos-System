f3fdec2d28381f0aae1470b9682aaf86782d3ded
Reef-hash <riffsyaz@gmail.com>
2026-05-06 22:53:53 +0800

    chore: bump version to next release

--- obj/Debug/net6.0-windows7.0/App.g.cs
+++ obj/Debug/net6.0-windows7.0/App.g.cs
@@ -52,7 +52,7 @@ namespace EZPos {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/EZPos;V1.0.1.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/EZPos;V1.0.2.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
